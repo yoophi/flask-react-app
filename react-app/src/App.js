fd1914606fbe888c1b0ec73ab545acf9b331c899
Pyunghyuk Yoo <yoophi@gmail.com>
2019-01-04 17:49:26 +0900

    react-app 에 개발용 proxy 세팅, API call test, 불필요 파일 삭제

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: {}
+    };
+  }
+
+  componentDidMount() {
+    axios.get("/api/v1.0/sample").then(resp => {
+      console.log(resp.data);
+      this.setState({ response: resp.data });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h3>API Response</h3>
+        <pre>{JSON.stringify(this.state.response, null, 2)}</pre>
       </div>
     );
   }
